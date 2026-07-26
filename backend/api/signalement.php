2430f1571d5f3734366eacb42ceba841f8ccbe88
farounoufous <noufousyemi@gmail.com>
2026-07-26 12:46:39 +0100

    Ajout de la carte)

--- backend/api/signalement.php
+++ backend/api/signalement.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 header("Access-Control-Allow-Origin: https://clearway-phi.vercel.app"); // ⚠️ REMPLACEZ par votre vraie URL Vercel
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
